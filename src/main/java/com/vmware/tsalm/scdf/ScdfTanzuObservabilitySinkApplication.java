ea5f21da46b373c32cdf27a0e8f323c09f0830f4
Timo Salm <tsalm@pivotal.io>
2020-04-27 14:01:42 +0200

    Initial project setup

---
+++ src/main/java/com/vmware/tsalm/scdf/ScdfTanzuObservabilitySinkApplication.java
@@ -0,0 +1,13 @@
+package com.vmware.tsalm.scdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScdfTanzuObservabilitySinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScdfTanzuObservabilitySinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vmware/tsalm/scdf/ScdfTanzuObservabilitySinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmware.tsalm.scdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScdfTanzuObservabilitySinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
